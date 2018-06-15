66b54a9e8e7d99b4ea007c911ada5d4182422583
Wendly Saintil <wendlysaintil@gmail.com>
2018-06-15 10:41:07 -0700

    published

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ const generator = require('../dist/index');
 const program = require('commander');
  
 program
-  .version('0.0.1')
+  .version('0.2.0')
   .command('[name]', 'name of the TS module to generate.')
   .option('-c, --config [config]', 'Path for the config file')
   .option('-d, --destination [destination]', 'Where to generates the files.')
